a3ad71a1d0ba73fec7afdf381f473d3c82efcc4f
FavioMancuso <faviomancusov@gmail.com>
2022-08-30 18:57:05 -0300

    Navbar

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,24 +1,14 @@
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import NavBar from './components/NavBar';
 
 function App() {
-
-  const estilos = {
-    width: "15cm"
-  }
   
   return (
-    <>
-    <NavBar/>
       <div className="App">
-        <h1 className="title" style={
-          {fontSize: "5rem"}
-          }>Camilita te amo mucho!</h1>
-          <p style={{
-            fontSize: "5rem"
-          }}>Vamos a dormir?</p>
-          <input placeholder="Ingrese su respuesta" style={estilos}></input> 
+        <Router>
+          <NavBar/>
+        </Router>
       </div>
-    </> 
   );
 }
 
