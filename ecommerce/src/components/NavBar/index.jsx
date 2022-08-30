6a9c64f072ca93d731a16df3901961759994c7dc
FavioMancuso <faviomancusov@gmail.com>
2022-08-30 18:30:18 -0300

    Navbar

--- ecommerce/src/components/NavBar/index.jsx
+++ ecommerce/src/components/NavBar/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {FaApple} from 'react-icons/fa'
 
 import './styles.css';
 
@@ -8,7 +9,7 @@ const NavBar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="#">YourPhone</Link>
+                    <Link className="navbar-brand align-contents-center" to="#">YourPhone<FaApple className='fs-4 mb-2'/></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
